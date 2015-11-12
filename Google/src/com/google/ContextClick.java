67680a69da9cf9c1a2b9aef6dec3f19fdbc07614
sChaklader <shchaklader@gmail.com>
2015-11-12 07:26:22 -0500

    context click

---
+++ Google/src/com/google/ContextClick.java
@@ -0,0 +1,26 @@
+package com.google;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class ContextClick {
+
+	public static void main(String[] args) {
+		WebDriver driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.get("https://www.google.com");
+		
+		WebElement link =driver.findElement(By.className("_Gs"));
+		Actions action = new Actions(driver);
+		action.contextClick(link).perform();
+		action.sendKeys("t").perform();
+
+
+	}
+
+}
